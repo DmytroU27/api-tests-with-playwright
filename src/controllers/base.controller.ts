6c22b86f35e012155faff1cbcd44a9e5c714ee4f
Dmytro <ukrainskiy1995d@gmail.com>
2026-02-22 11:35:18 +0200

    Make BaseController abstract

--- src/controllers/base.controller.ts
+++ src/controllers/base.controller.ts
@@ -1,5 +1,5 @@
 import { APIRequestContext } from "@playwright/test";
 
-export class BaseController {
+export abstract class BaseController {
     constructor(protected apiKey: string, protected request: APIRequestContext) { }
 }
